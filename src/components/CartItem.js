2666e1cf0b3704650df4aee5291a2871e3e55a1d
Pedro-Arantes <developer9pedro@gmail.com>
2022-11-24 15:16:29 -0300

    feat: CartItem SumSubAmount function

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,24 +1,73 @@
 import { useState, useContext, useEffect } from 'react';
-
+import axios from "axios";
 import {  CartItemStyled, CartDescription,CounterDiv,CartContent } from "../assets/styles/CartStyle"
 import {FiTrash2} from  'react-icons/fi'
 
-export default function CartItem (){
+export default function CartItem ({objt,setUpdate,token,navigate}){
 
-    
+    const {img,model,price,amount,_id} = objt
+    const real =  Number(price).toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})
+    let qnt = !amount ? 1 : amount
+    qnt = Number(qnt)
+
+    let num= Number(qnt) ;
+   
+    const updateAmount = ()=>{
+        const obj ={
+            amount: num,
+            id: _id
+        }
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+        const URL = 'http://localhost:5000/cart'
+        const tratarSucesso = (resposta) => {
+            //console.log(resposta)  
+        }
+
+        const tratarErro = (resp) => {
+            //console.log(resp)
+            alert(resp.response.data.message)
+            navigate("/")
+        }
+
+        const requisicao = axios.put(URL, obj, config);
+        requisicao.then(tratarSucesso)
+        requisicao.catch(tratarErro)
+    }
+
+    const SumSubAmount = (operator) =>{
+        
+            if (operator === "minus") {
+                if (qnt === 1) {
+                    num= 1
+                }else{
+                    num= qnt-1
+
+                    
+                }
+            } else {
+                num= qnt+1
+            }
+            setUpdate(num)
+            updateAmount()
+
+    }
 
     return(
         <CartItemStyled>
         <CartContent>
-            <img alt="vazio" src="http://cdn.shopify.com/s/files/1/0413/8896/6039/products/celular-samsung-galaxy-a71-128gb-6gb-ram-dual-tela-67-4g-4-cameras-preto_1.jpg?v=1601306627"></img>
+            <img alt="vazio" src={img}></img>
 
             <CartDescription>
-                <p>Sansung Galaxy A71</p>
-                <p>$64,95</p>
+                <p>{model}</p>
+                <p>{real}</p>
                 <CounterDiv>
-                    <button>-</button>
-                    <p>1</p>
-                    <button>+</button>
+                    <button onClick={()=>SumSubAmount("minus")}>-</button>
+                    <p>{qnt?.toString()}</p>
+                    <button onClick={()=>SumSubAmount("plus")}>+</button>
                 </CounterDiv>
                 
             </CartDescription>
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -9,20 +9,52 @@ import NavBarComponent from "../components/NavBarComponent";
 export default function CartPage() {
 
     const [total, setTotal] = useState("")
-    const { setCart} = useContext(DataContext)
+    const [update,setUpdate] = useState("")
+    const { setCart,cartArray} = useContext(DataContext)
     const array = [1,2,3,4,5,6,7]
     const navigate = useNavigate();
-
+    const token = "540e441c-9227-4749-ba22-9fe8204e7dfc"
     const NextPage = ()=>{
         //verificar se o carrinho está vazio
         navigate("/compra")
     }
+
+    useEffect(()=>{
+        const getCart = ()=>{
+
+            const URL = 'http://localhost:5000/cart'
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            const tratarSucesso = (resp) => {
+               
+                const dataArray = resp.data.cart
+                
+                setCart(dataArray)
+            }
+
+            const tratarErro = (resp) => {
+                console.log(resp)
+                alert(resp.response.data)
+                navigate("/")
+                window.location.reload()
+            }
+
+            const req = axios.get(URL, config);
+            req.then(tratarSucesso)
+            req.catch(tratarErro)
+        }
+        getCart()
+    },[update])
+
     return (
         <MainStyled>
             <NavBarComponent/>
             <CartStyled>
                 <h1>Bem vindo ao seu carrinho!</h1>
-                {array.map((item,id)=><CartItem key={id}/>)}
+                {cartArray?.map((item,id)=><CartItem navigate={navigate} setUpdate={setUpdate} objt={item} token={token}key={id}/>)}
                 
             </CartStyled>
             <BalanceStyled>

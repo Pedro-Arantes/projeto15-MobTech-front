bcf5aeb0997d29ef7c904fe4937f466f58c43e67
Pedro Arantes <developer9pedro@gmail.com>
2022-11-28 17:58:28 -0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -10,14 +10,14 @@ export const SIGN_UP_URL = 'http://localhost:5000/sign_up';
 
 export const FAVS_URL = 'http://localhost:5000/favorites'; */
 
-export const PRODUCTS_URL = 'https://mobtech.onrender.com/products';
+export const PRODUCTS_URL = 'https://mobtech-faor.onrender.com/products';
 
-export const CART_URL = 'https://mobtech.onrender.com/cart';
+export const CART_URL = 'https://mobtech-faor.onrender.com/cart';
 
-export const PURCHASE_URL = 'https://mobtech.onrender.com/purchase';
+export const PURCHASE_URL = 'https://mobtech-faor.onrender.com/purchase';
 
-export const SIGN_IN_URL = 'https://mobtech.onrender.com/sign_in';
+export const SIGN_IN_URL = 'https://mobtech-faor.onrender.com/sign_in';
 
-export const SIGN_UP_URL = 'https://mobtech.onrender.com/sign_up';
+export const SIGN_UP_URL = 'https://mobtech-faor.onrender.com/sign_up';
 
-export const FAVS_URL = 'https://mobtech.onrender.com/favorites';
+export const FAVS_URL = 'https://mobtech-faor.onrender.com/favorites';
